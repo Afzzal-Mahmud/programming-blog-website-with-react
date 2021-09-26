831df5c6c18b16a0466eb327a4c670680ecc1def
afzal mahmud <afzzalmahmud@gmail.com>
2021-09-26 12:51:07 +0600

    add json data and done with header section

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Header.css"
+
+function Header() {
+    return(
+       <header>
+            <div className="container">
+            <nav>
+                <div className="logo">
+                    <h3 style={{color:"#e8e5e1"}}>TopTen.net</h3>
+                </div>
+                <div className="nav-link">
+                    <ul>
+                        <li><a href="/Home">Home</a></li>
+                        <li><a href="/About">About</a></li>
+                        <li><a href="/Services">Services</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+       </header>
+    )
+}
+export default Header;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import "./Home.css"
 
 function Home() {
+    const [blogs,setBlogs] = useState([]);
+  useEffect(()=>{
+    fetch('./blogs.json')
+    .then(Response => Response.json())
+    .then(data => setBlogs(data))
+  },[])
     return(
-        <div className="container">
-            
+      <>
+      <Header></Header>
+        <section className="container home-container">
+        <div className="dashbord-container">
+              {/* all blog data map hear */}
         </div>
+        </section>
+        </>
     )
 }
 
