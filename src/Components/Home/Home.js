16ddb5c0a3f9675766043fc0c68f333061521b7f
afzal mahmud <afzzalmahmud@gmail.com>
2021-09-26 15:00:39 +0600

    add card-dashbord-card and update json data

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from "react";
+import DashboardCard from "../Dashboard/Dashboard";
 import Header from "../Header/Header";
 import "./Home.css"
 
 function Home() {
-    const [blogs,setBlogs] = useState([]);
-  useEffect(()=>{
+  // fetching fake data from public folder
+  const [blogData, setBlogsData] = useState([]);
+  useEffect(() => {
     fetch('./blogs.json')
-    .then(Response => Response.json())
-    .then(data => setBlogs(data))
-  },[])
-    return(
-      <>
+      .then(Response => Response.json())
+      .then(data => setBlogsData(data))
+  }, [])
+  return (
+    <>
       <Header></Header>
-        <section className="container home-container">
-        <div className="dashbord-container">
-              {/* all blog data map hear */}
+
+        <h2 style={{textAlign:"center"}}>Top 10 Developer Blog I follow</h2>
+      <section className="container home-container">
+        <div className="dashboard-container">
+          {/* all blogs data map heare and send to DashboardCard as object */}
+        {
+          blogData.map(blog => <DashboardCard
+                               key={blog.id} 
+                               blogObject={blog}>
+                              </DashboardCard>)
+        }
+
         </div>
-        </section>
-        </>
-    )
+      </section>
+    </>
+  )
 }
 
 export default Home;
